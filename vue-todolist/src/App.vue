19748dfa537a4938b9277ef26e40fb841915122e
Hikari1149 <122094854@qq.com>
2019-04-14 13:01:19 +0800

    add vue-todoList

---
+++ vue-todolist/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <input v-model="inputVal"/>
+    <button v-on:click="handleAdd">add</button>
+    <ul>
+      <todoItem v-for="(item,index) of todoList"
+                v-bind:item="item"
+                v-bind:index="index"
+                v-bind:key="index"
+                v-on:delete="handleDelte"
+
+      >
+      </todoItem>
+    </ul>
+  </div>
+</template>
+
+<script>
+import TodoItem from './components/TodoItem.vue'
+
+export default {
+  name: 'app',
+  components: {
+    inputVal:'',
+    todoItem:TodoItem
+  },
+  data(){
+      return {
+          todoList:[11,22],
+      }
+  },
+  methods:{
+      handleAdd(){
+          this.todoList.push(this.inputVal);
+          this.inputVal='';
+      },
+      handleDelte(index){
+          this.todoList.splice(index,1);
+      }
+  }
+}
+/*
+ v-for循环展示数据. v-model=inputVal用于vm的双向绑定.
+ 将input数据添加进list v-on:click
+ 子组件使用this.$emit向父组件发布事件, 父组件v-on:(xxx)监听事件并处理.
+*/
+
+</script>
+<style>
+</style>
+
---
+++ vue-todolist/src/components/TodoItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <li class="TodoItem"
+      v-on:click="handleDelete"
+  >
+    {{item}} {{index}}
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'TodoItem',
+  props:['item','index'],
+  methods:{
+      handleDelete(){
+          this.$emit('delete',this.index);
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
